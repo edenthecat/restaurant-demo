8a0e2577a6a3742c3967d3608c143101a5e615e1
Eden Rohatensky <edenrohatensky@gmail.com>
2017-04-04 09:51:54 -0400

    Added seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "PFK", address: "Montreal", rating: 100)
+Restaurant.create(name: "McDonalds", address: "Yorkton", rating: 2)
